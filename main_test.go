98013595c5e30b91918624e8ec21b0d78bd82546
Nikolay Dubina <nikolay.dubina.pub@gmail.com>
2024-03-26 16:36:14 +0800

    cmd (#5)
    * cmd
    
    * fix

--- main_test.go
+++ main_test.go
@@ -1,32 +1,45 @@
-package main
+package main_test
 
 import (
 	"os"
+	"os/exec"
 	"testing"
 )
 
 func TestMain(t *testing.T) {
-	if err := process("Color", "internal/testdata/color.go", "main"); err != nil {
-		t.Fatal(err)
+	cmd := exec.Command("./main.test", "--type", "Color")
+	cmd.Env = append(cmd.Environ(), "GOFILE=internal/testdata/color.go", "GOPACKAGE=main", "GOCOVERDIR=.")
+
+	if _, err := cmd.Output(); err != nil {
+		t.Fatalf("cannot run main: %s", err)
 	}
 
 	assertEqFile(t, "internal/testdata/color_enum_encoding.go", "internal/testdata/exp/color_enum_encoding.go")
 	assertEqFile(t, "internal/testdata/color_enum_encoding_test.go", "internal/testdata/exp/color_enum_encoding_test.go")
 
 	t.Run("when wrong params, then error", func(t *testing.T) {
-		if err := process("", "internal/testdata/color.go", "main"); err == nil {
+		cmd := exec.Command("./main.test")
+		cmd.Env = append(cmd.Environ(), "GOFILE=internal/testdata/color.go", "GOPACKAGE=main", "GOCOVERDIR=.")
+
+		if _, err := cmd.Output(); err == nil {
 			t.Fatal("must be error")
 		}
 	})
 
 	t.Run("when no undefined, then error", func(t *testing.T) {
-		if err := process("NoUndefined", "internal/testdata/no_undefined.go", "main"); err == nil {
+		cmd := exec.Command("./main.test", "--type", "NoUndefined")
+		cmd.Env = append(cmd.Environ(), "GOFILE=internal/testdata/no_undefined.go", "GOPACKAGE=main", "GOCOVERDIR=.")
+
+		if _, err := cmd.Output(); err == nil {
 			t.Fatal("must be error")
 		}
 	})
 
 	t.Run("when undefined has wrong tag, then error", func(t *testing.T) {
-		if err := process("BadUndefined1", "internal/testdata/wrong_tag_undefined.go", "main"); err == nil {
+		cmd := exec.Command("./main.test", "--type", "BadUndefined1")
+		cmd.Env = append(cmd.Environ(), "GOFILE=internal/testdata/wrong_tag_undefined.go", "GOPACKAGE=main", "GOCOVERDIR=.")
+
+		if _, err := cmd.Output(); err == nil {
 			t.Fatal("must be error")
 		}
 	})
