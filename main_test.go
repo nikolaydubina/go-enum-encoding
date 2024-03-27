4f85ab99621a99be36602809cc3536f92e4e69b1
Nikolay Dubina <nikolay.dubina.pub@gmail.com>
2024-03-27 19:35:22 +0800

    fuzz-bad (#16)
    * fuzz-bad
    
    * pin

--- main_test.go
+++ main_test.go
@@ -7,6 +7,24 @@ import (
 	"testing"
 )
 
+func FuzzBadFile(f *testing.F) {
+	testbin := path.Join(f.TempDir(), "go-enum-encoding-test")
+	exec.Command("go", "build", "-cover", "-o", testbin, "main.go").Run()
+
+	f.Fuzz(func(t *testing.T, orig string) {
+		t.Run("when bad go file, then error", func(t *testing.T) {
+			fname := path.Join(t.TempDir(), "fuzz-test-file.go")
+			os.WriteFile(fname, []byte(orig), 0644)
+
+			cmd := exec.Command(testbin, "--type", "Color")
+			cmd.Env = append(cmd.Environ(), "GOFILE="+fname, "GOPACKAGE=main")
+			if err := cmd.Run(); err == nil {
+				t.Fatal("must be error")
+			}
+		})
+	})
+}
+
 func TestMain(t *testing.T) {
 	coverdir := t.TempDir()
 	testbin := path.Join(t.TempDir(), "go-enum-encoding-test")
