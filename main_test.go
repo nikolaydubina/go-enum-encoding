9d50e854dc9abb2579e1a788e6a461e6a6a0985f
Nikolay Dubina <nikolay.dubina.pub@gmail.com>
2024-03-28 15:26:30 +0800

    fix-test

--- main_test.go
+++ main_test.go
@@ -40,6 +40,17 @@ func TestMain(t *testing.T) {
 		assertEqFile(t, "internal/testdata/color_enum_encoding_test.go", "internal/testdata/exp/color_enum_encoding_test.go")
 	})
 
+	t.Run("when no undefined, then ok", func(t *testing.T) {
+		cmd := exec.Command(testbin, "--type", "NoUndefined")
+		cmd.Env = append(cmd.Environ(), "GOFILE=internal/testdata/no_undefined.go", "GOPACKAGE=main", "GOCOVERDIR="+coverdir)
+		if err := cmd.Run(); err != nil {
+			t.Fatal("must be error")
+		}
+
+		assertEqFile(t, "internal/testdata/noundefined_enum_encoding.go", "internal/testdata/exp/noundefined_enum_encoding.go")
+		assertEqFile(t, "internal/testdata/noundefined_enum_encoding_test.go", "internal/testdata/exp/noundefined_enum_encoding_test.go")
+	})
+
 	t.Run("when bad go file, then error", func(t *testing.T) {
 		cmd := exec.Command(testbin, "--type", "Color")
 		cmd.Env = append(cmd.Environ(), "GOFILE=README.md", "GOPACKAGE=main", "GOCOVERDIR="+coverdir)
@@ -68,14 +79,6 @@ func TestMain(t *testing.T) {
 		cmd := exec.Command(testbin)
 		cmd.Env = append(cmd.Environ(), "GOFILE=internal/testdata/color.go", "GOPACKAGE=main", "GOCOVERDIR="+coverdir)
 		if err := cmd.Run(); err == nil {
-			t.Fatal("must be error")
-		}
-	})
-
-	t.Run("when no undefined, then error", func(t *testing.T) {
-		cmd := exec.Command(testbin, "--type", "NoUndefined")
-		cmd.Env = append(cmd.Environ(), "GOFILE=internal/testdata/no_undefined.go", "GOPACKAGE=main", "GOCOVERDIR="+coverdir)
-		if err := cmd.Run(); err != nil {
 			t.Fatal("must be error")
 		}
 	})
