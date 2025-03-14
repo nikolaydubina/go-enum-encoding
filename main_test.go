94dfb3e00e8ed6eada26470df5574699f7414390
Nikolay Dubina <nikolay.dubina.pub@gmail.com>
2025-03-14 13:43:29 +0800

    fix

--- main_test.go
+++ main_test.go
@@ -38,7 +38,7 @@ func TestMain(t *testing.T) {
 
 		t.Run("struct", func(t *testing.T) {
 			cmd := exec.Command(testbin, "--type", "Color")
-			cmd.Env = append(cmd.Environ(), "GOFILE="+filepath.Join(testdir, "image.go"), "GOLINE=4", "GOPACKAGE=image", "GOTESTDIR="+testdir, "GOCOVERDIR="+os.Getenv("GOCOVERDIR"))
+			cmd.Env = append(cmd.Environ(), "GOFILE="+filepath.Join(testdir, "image.go"), "GOLINE=4", "GOPACKAGE=image", "GOTESTDIR="+testdir, "GOCOVERDIR="+testdir)
 			if err := cmd.Run(); err != nil {
 				t.Error(err)
 			}
@@ -49,7 +49,7 @@ func TestMain(t *testing.T) {
 
 		t.Run("iota, string", func(t *testing.T) {
 			cmd := exec.Command(testbin, "--type", "ImageSize", "--string")
-			cmd.Env = append(cmd.Environ(), "GOFILE="+filepath.Join(testdir, "image.go"), "GOLINE=18", "GOPACKAGE=image", "GOTESTDIR="+testdir, "GOCOVERDIR="+os.Getenv("GOCOVERDIR"))
+			cmd.Env = append(cmd.Environ(), "GOFILE="+filepath.Join(testdir, "image.go"), "GOLINE=18", "GOPACKAGE=image", "GOTESTDIR="+testdir, "GOCOVERDIR="+testdir)
 			if err := cmd.Run(); err != nil {
 				t.Error(err)
 			}
@@ -75,7 +75,7 @@ func TestMain(t *testing.T) {
 		exec.Command("cp", filepath.Join("internal", "README.md"), filepath.Join(testdir, "README.md")).Run()
 
 		cmd := exec.Command(testbin, "--type", "Color")
-		cmd.Env = append(cmd.Environ(), "GOFILE=README.md", "GOLINE=5", "GOPACKAGE=image", "GOTESTDIR="+testdir, "GOCOVERDIR="+os.Getenv("GOCOVERDIR"))
+		cmd.Env = append(cmd.Environ(), "GOFILE=README.md", "GOLINE=5", "GOPACKAGE=image", "GOTESTDIR="+testdir, "GOCOVERDIR="+testdir)
 		if err := cmd.Run(); err == nil {
 			t.Fatal("must be error")
 		}
@@ -83,7 +83,7 @@ func TestMain(t *testing.T) {
 
 	t.Run("when enum values not immediately after go:generate line, then error", func(t *testing.T) {
 		cmd := exec.Command(testbin, "--type", "Color")
-		cmd.Env = append(cmd.Environ(), "GOFILE="+filepath.Join(testdir, "image.go"), "GOLINE=1", "GOPACKAGE=image", "GOTESTDIR="+testdir, "GOCOVERDIR="+os.Getenv("GOCOVERDIR"))
+		cmd.Env = append(cmd.Environ(), "GOFILE="+filepath.Join(testdir, "image.go"), "GOLINE=1", "GOPACKAGE=image", "GOTESTDIR="+testdir, "GOCOVERDIR="+testdir)
 		if err := cmd.Run(); err == nil {
 			t.Fatal("must be error")
 		}
@@ -91,7 +91,7 @@ func TestMain(t *testing.T) {
 
 	t.Run("when invalid package name, then error", func(t *testing.T) {
 		cmd := exec.Command(testbin, "--type", "Color")
-		cmd.Env = append(cmd.Environ(), "GOFILE="+filepath.Join(testdir, "image.go"), "GOLINE=5", "GOPACKAGE=\"", "GOTESTDIR="+testdir, "GOCOVERDIR="+os.Getenv("GOCOVERDIR"))
+		cmd.Env = append(cmd.Environ(), "GOFILE="+filepath.Join(testdir, "image.go"), "GOLINE=5", "GOPACKAGE=\"", "GOTESTDIR="+testdir, "GOCOVERDIR="+testdir)
 		if err := cmd.Run(); err == nil {
 			t.Fatal("must be error")
 		}
@@ -99,7 +99,7 @@ func TestMain(t *testing.T) {
 
 	t.Run("when not found file, then error", func(t *testing.T) {
 		cmd := exec.Command(testbin, "--type", "Color")
-		cmd.Env = append(cmd.Environ(), "GOFILE=asdf.asdf", "GOPACKAGE=image", "GOLINE=5", "GOTESTDIR="+testdir, "GOCOVERDIR="+os.Getenv("GOCOVERDIR"))
+		cmd.Env = append(cmd.Environ(), "GOFILE=asdf.asdf", "GOPACKAGE=image", "GOLINE=5", "GOTESTDIR="+testdir, "GOCOVERDIR="+testdir)
 		if err := cmd.Run(); err == nil {
 			t.Fatal("must be error")
 		}
@@ -107,7 +107,7 @@ func TestMain(t *testing.T) {
 
 	t.Run("when wrong params, then error", func(t *testing.T) {
 		cmd := exec.Command(testbin)
-		cmd.Env = append(cmd.Environ(), "GOFILE="+filepath.Join(testdir, "image.go"), "GOLINE=5", "GOPACKAGE=color", "GOTESTDIR="+testdir, "GOCOVERDIR="+os.Getenv("GOCOVERDIR"))
+		cmd.Env = append(cmd.Environ(), "GOFILE="+filepath.Join(testdir, "image.go"), "GOLINE=5", "GOPACKAGE=color", "GOTESTDIR="+testdir, "GOCOVERDIR="+testdir)
 		if err := cmd.Run(); err == nil {
 			t.Fatal("must be error")
 		}
